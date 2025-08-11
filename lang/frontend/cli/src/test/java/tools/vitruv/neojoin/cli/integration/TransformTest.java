29fc834ff78c80c8a8745ad75e5e160b30bd8e4e
larsk21 <57503246+larsk21@users.noreply.github.com>
2025-08-11 14:33:04 +0200

    add CLI integration test for transforming instance models

---
+++ lang/frontend/cli/src/test/java/tools/vitruv/neojoin/cli/integration/TransformTest.java
@@ -0,0 +1,70 @@
+package tools.vitruv.neojoin.cli.integration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static tools.vitruv.neojoin.cli.integration.Utils.compareInstanceFiles;
+import static tools.vitruv.neojoin.cli.integration.Utils.getResource;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.eclipse.emf.compare.Diff;
+import org.eclipse.emf.compare.DifferenceKind;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.io.TempDir;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.FieldSource;
+
+import picocli.CommandLine;
+import tools.vitruv.neojoin.cli.Main;
+
+public class TransformTest {
+    
+    static List<String> validQueries = List.of("pizza");
+
+    @BeforeAll
+    public static void setupRegistry() {
+        if (!Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().containsKey("xmi")) {
+            Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("xmi", new XMIResourceFactoryImpl());
+        }
+    }
+
+    @ParameterizedTest
+    @FieldSource("validQueries")
+    public void testTransformInputModels(String queryName, @TempDir Path outputDirectory) throws URISyntaxException, IOException {
+        // GIVEN meta-models, instance models, a valid query and an output path
+        URL metaModelPath = getResource("models");
+        URL instanceModelPath = getResource("instances");
+        URL query = getResource("queries/" + queryName + ".nj");
+
+        Path output = outputDirectory.resolve(queryName + ".xmi");
+
+        String metaModelPathArg = "--meta-model-path=" + metaModelPath.toURI();
+        String instanceModelPathArg = "--instance-model-path=" + instanceModelPath.toURI();
+        String transformArg = "--transform=" + output;
+        String queryArg = query.getPath();
+
+        // WHEN transforming the instance models
+        int exitCode = new CommandLine(new Main()).execute(new String[] { metaModelPathArg, instanceModelPathArg, transformArg, queryArg });
+
+        // THEN the correct view is generated (ignoring the order of view elements)
+        assertEquals(0, exitCode);
+        
+        URL resultModel = getResource("models/" + queryName + ".ecore");
+        URL result = getResource("results/" + queryName + ".xmi");
+
+        Stream<Diff> differences = compareInstanceFiles(Path.of(resultModel.toURI()), Path.of(result.toURI()), output)
+            .getDifferences()
+            .stream()
+            .filter(diff -> diff.getKind() != DifferenceKind.MOVE);
+
+        assertTrue(differences.findAny().isEmpty());
+    }
+
+}
