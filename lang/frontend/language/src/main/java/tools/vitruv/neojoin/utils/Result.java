db0549e27a4805291b592e70d6466ee2b3269af2
Tobias Stickling <uudyh@student.kit.edu>
2025-11-06 15:41:22 +0100

    Allow nullable values in Result<T> type
    Fixes warning `Non-null type argument is expected` for usages like `Result<@Nullable Object>`

--- lang/frontend/language/src/main/java/tools/vitruv/neojoin/utils/Result.java
+++ lang/frontend/language/src/main/java/tools/vitruv/neojoin/utils/Result.java
@@ -1,12 +1,14 @@
 package tools.vitruv.neojoin.utils;
 
+import org.jspecify.annotations.Nullable;
+
 /**
  * Represents the result of an operation that can either {@link Success succeed} by returning a value or {@link Failure fail}
  * with an exception.
  *
  * @param <T> type of the return value if successful
  */
-public sealed interface Result<T> {
+public sealed interface Result<T extends @Nullable Object> {
 
     record Success<T>(
         T value
