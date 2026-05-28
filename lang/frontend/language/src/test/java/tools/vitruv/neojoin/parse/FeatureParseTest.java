eed59e160932c5b954aa1d2898561fb19c9d4933
pxxx <p_xxx@tutanota.com>
2026-05-28 19:05:16 +0200

    Implement more test.

--- lang/frontend/language/src/test/java/tools/vitruv/neojoin/parse/FeatureParseTest.java
+++ lang/frontend/language/src/test/java/tools/vitruv/neojoin/parse/FeatureParseTest.java
@@ -10,6 +10,8 @@ import tools.vitruv.neojoin.jvmmodel.ExpressionHelper;
 import tools.vitruv.neojoin.jvmmodel.TypeInfo;
 import tools.vitruv.neojoin.jvmmodel.TypeResolutionException;
 
+import java.util.List;
+
 import static java.util.Objects.requireNonNull;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.*;
@@ -17,6 +19,7 @@ import static tools.vitruv.neojoin.parse.ParseAssertions.assertThat;
 
 @SuppressWarnings("HttpUrlsUsage")
 class FeatureParseTest extends AbstractParseTest {
+    private static final String FAILED_TO_RETRIEVE_TYPE_MESSAGE_PREFIX = "Failed to retrieve inferred type of feature:";
 
     @Test
     void classAliasMissing() {
@@ -192,11 +195,95 @@ class FeatureParseTest extends AbstractParseTest {
         assertThat(result).hasIssues("Copy feature expression does not reference a feature");
     }
 
-
     @Test
     void inferUnboxedType() {
         var expressionHelper = getInjector().getInstance(ExpressionHelper.class);
-        var result = internalParse("""
+        var fixtures = List.of(
+            new CheckInferredFeatureTypeFixture(
+                "year",
+                "year := films.same[ it.year ]",
+                EcorePackage.Literals.EINT
+            ),
+            new CheckInferredFeatureTypeFixture(
+                "n",
+                "n := 42",
+                EcorePackage.Literals.EINT
+            ),
+            new CheckInferredFeatureTypeFixture(
+                "isItTheYear",
+                "isItTheYear := true",
+                EcorePackage.Literals.EBOOLEAN
+            ),
+            new CheckInferredFeatureTypeFixture(
+            "it",
+            "it := 'helo world'.charAt(0)",
+                EcorePackage.Literals.ECHAR
+            ),
+            new CheckInferredFeatureTypeFixture(
+                "it",
+                "it := 'helo world'.getBytes().get(2)",
+                EcorePackage.Literals.EBYTE
+            )
+        );
+
+        for (var fixture : fixtures) {
+            runCheckInferredFeatureTypeTest(fixture);
+        }
+    }
+
+    @Test
+    void inferListOfPrimitives() {
+        runCheckInferredFeatureTypeTest(
+            new CheckInferredFeatureTypeFixture(
+                "years",
+                "years := films.map[ it.year ]",
+                EcorePackage.Literals.EINTEGER_OBJECT,
+                true
+            )
+        );
+    }
+
+    @Test
+    void inferByteArray() {
+        runCheckInferredFeatureTypeTest(
+            new CheckInferredFeatureTypeFixture(
+                "it",
+                "it := 'foobar'.getBytes()",
+                EcorePackage.Literals.EBYTE_ARRAY
+            )
+        );
+    }
+
+    private void runCheckInferredFeatureTypeTest(CheckInferredFeatureTypeFixture fixture) {
+        assertThatNoException()
+        .isThrownBy(super::setUpAIT);
+
+        var expressionHelper = getInjector().getInstance(ExpressionHelper.class);
+        var result = internalParse(
+            filmSummeryPerYearQuery(fixture.featureDeclaration)
+        );
+
+        assertThat(result)
+            .hasNoIssues();
+
+        var query = result.left().getQueries().get(0);
+        var feature = getFeatureOrFail(query, fixture.featureName);
+        var featureType = inferredTypeOrFail(expressionHelper, feature.getExpression());
+        var featureClassifier = getClassifierOrFail(featureType);
+
+        assertThat(feature.getType())
+            .as("Type should not have ben assigned.")
+            .isNull();
+
+        assertThat(featureType.isMany())
+            .isEqualTo(fixture.shouldBeMany);
+
+        assertThat(featureClassifier)
+            .isEqualTo(fixture.expectedClassifier);
+    }
+
+    private String filmSummeryPerYearQuery(String summeryBody) {
+        return """
             export package to "http://example.com"
 
             import "http://example.org/imdb"
@@ -204,19 +291,25 @@ class FeatureParseTest extends AbstractParseTest {
             from Film films
             group by films.year
             create Summary {
-                year := films.same[ it.year ]
+                %s
             }
-            """);
+            """.formatted(summeryBody);
+    }
 
-        assertThat(result)
-            .hasNoIssues();
+    private record CheckInferredFeatureTypeFixture(
+        String featureName,
+        String featureDeclaration,
+        EClassifier expectedClassifier,
+        Boolean shouldBeMany
+    ) {
 
-        var query = result.left().getQueries().get(0);
-        var yearFeature = getFeatureOrFail(query, "year");
-        var featureClassifier = inferredClassifierOrFail(expressionHelper, yearFeature.getExpression());
-
-        assertThat(featureClassifier)
-            .isEqualTo(EcorePackage.Literals.EINT);
+        CheckInferredFeatureTypeFixture(
+            String featureName,
+            String featureDeclaration,
+            EClassifier expectedClassifier
+        ) {
+            this(featureName, featureDeclaration, expectedClassifier, false);
+        }
     }
 
     private Feature getFeatureOrFail(Query query, String featureName) {
@@ -227,21 +320,22 @@ class FeatureParseTest extends AbstractParseTest {
                 .orElseGet(() -> fail("No feature with name: '%s' found in query.".formatted(featureName))));
     }
 
-    private EClassifier inferredClassifierOrFail(ExpressionHelper expressionHelper, XExpression expression) {
-        final String FAILURE_MESSAGE_PREFIX = "Failed to retrieve inferred type of feature:";
+    private EClassifier getClassifierOrFail(TypeInfo typeInfo) {
+        EClassifier classifier = typeInfo.classifier();
 
+        return classifier == null ? fail("%s Classifier was null.".formatted(FAILED_TO_RETRIEVE_TYPE_MESSAGE_PREFIX))
+            : classifier;
+    }
+
+    private TypeInfo inferredTypeOrFail(ExpressionHelper expressionHelper, XExpression expression) {
         TypeInfo typeInfo;
         try {
             typeInfo = expressionHelper.inferEType(expression);
         } catch (TypeResolutionException e) {
-            typeInfo = fail(FAILURE_MESSAGE_PREFIX, e);
+            typeInfo = fail(FAILED_TO_RETRIEVE_TYPE_MESSAGE_PREFIX, e);
         }
 
-        EClassifier classifier =
-            typeInfo == null ? fail("%s Type info was null.".formatted(FAILURE_MESSAGE_PREFIX))
-                : typeInfo.classifier();
-
-        return classifier == null ? fail("%s Classifier was null.".formatted(FAILURE_MESSAGE_PREFIX))
-            : classifier;
+        return typeInfo == null ? fail("%s Type info was null.".formatted(FAILED_TO_RETRIEVE_TYPE_MESSAGE_PREFIX))
+                : typeInfo;
     }
 }
