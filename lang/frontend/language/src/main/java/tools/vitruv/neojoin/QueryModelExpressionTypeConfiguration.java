4be10f91a2dd749f26a71c8a5aa46d2ed6e62316
Jean Patrick Mathes <jeanpatrick.mathes@gmx.de>
2026-08-16 18:02:25 +0200

    Add some javadoc comments

--- lang/frontend/language/src/main/java/tools/vitruv/neojoin/QueryModelExpressionTypeConfiguration.java
+++ lang/frontend/language/src/main/java/tools/vitruv/neojoin/QueryModelExpressionTypeConfiguration.java
@@ -4,6 +4,9 @@ import com.google.inject.ImplementedBy;
 import org.eclipse.xtext.common.types.JvmGenericType;
 import tools.vitruv.neojoin.ast.ViewTypeDefinition;
 
+/**
+ * Configures the type holding all methods which are created for the expressions in the query.
+ */
 @ImplementedBy(DefaultQueryModelExpressionTypeConfiguration.class)
 public interface QueryModelExpressionTypeConfiguration {
     void configure(JvmGenericType queryModelExpressionType, ViewTypeDefinition viewType);
--- lang/frontend/language/src/main/java/tools/vitruv/neojoin/jvmmodel/SourceTypeRegistryInitialization.java
+++ lang/frontend/language/src/main/java/tools/vitruv/neojoin/jvmmodel/SourceTypeRegistryInitialization.java
@@ -5,6 +5,9 @@ import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.xtext.xbase.jvmmodel.JvmTypeReferenceBuilder;
 
+/**
+ * Initializes a type registry to contain the types of the view source metamodels.
+ */
 @ImplementedBy(InferringSourceTypeRegistryInitialization.class)
 public interface SourceTypeRegistryInitialization {
     void initialize(TypeRegistry typeRegistry, EPackage.Registry packageRegistry, JvmTypeReferenceBuilder typeReferenceBuilder, ResourceSet resourceSet);
